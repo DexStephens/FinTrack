e3e8e064fa417479bec4b257cee6e068bc206769
Dexter Stephens <dexterfstephens@gmail.com>
2023-01-21 11:52:37 -0700

    Add the div styling to center and make the footer sticky

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,17 +7,35 @@ import { NewUserForm } from './Profile/NewUserForm';
 import { LoginForm } from './Profile/LoginForm';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './Home/Header';
+import { Footer } from './Home/Footer';
 function App() {
   return (
     <BrowserRouter>
-      <h1>This is the header</h1>
-      <Routes>
-        <Route path="" element={<HomePage />} />
-        <Route path="profile" element={<ProfilePage />}>
-          <Route path="login" element={<LoginForm />} />
-          <Route path="newUser" element={<NewUserForm />} />
-        </Route>
-      </Routes>
+      <div
+        css={css`
+          min-height: 100vh;
+          margin: 0;
+          padding: 0;
+          display: flex;
+          flex-direction: column;
+        `}
+      >
+        <Header />
+        <div
+          css={css`
+            flex-grow: 1;
+          `}
+        >
+          <Routes>
+            <Route path="" element={<HomePage />} />
+            <Route path="profile" element={<ProfilePage />}>
+              <Route path="login" element={<LoginForm />} />
+              <Route path="newUser" element={<NewUserForm />} />
+            </Route>
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
